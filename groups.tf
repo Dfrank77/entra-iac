5b707660da938dabaff3566d75329338d062c038
Darius Frank <55293358+Dfrank77@users.noreply.github.com>
2026-06-10 06:15:05 -0600

    Update groups.tf

--- groups.tf
+++ groups.tf
@@ -2,12 +2,17 @@ resource "azuread_group" "iam_engineers" {
   display_name     = "IaC - IAM Engineers"
   security_enabled = true
   mail_enabled     = false
-  members          = [azuread_user.lab_user_1.object_id]
+  members = [
+    azuread_user.lab_users["iac.user1"].object_id,
+    azuread_user.lab_users["iac.user3"].object_id,
+  ]
 }
 
 resource "azuread_group" "helpdesk_staff" {
   display_name     = "IaC - Helpdesk Staff"
   security_enabled = true
   mail_enabled     = false
-  members          = [azuread_user.lab_user_2.object_id]
+  members = [
+    azuread_user.lab_users["iac.user2"].object_id,
+  ]
 }
