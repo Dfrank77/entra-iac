b8db5073ff2e049b28032ca5962df144fd9b53bf
Darius Frank <55293358+Dfrank77@users.noreply.github.com>
2026-06-10 06:13:37 -0600

    Update users.tf

--- users.tf
+++ users.tf
@@ -1,17 +1,37 @@
-resource "azuread_user" "lab_user_1" {
-  user_principal_name = "iac.user1@d${var.domain_name}"
-  display_name        = "IaC Lab User 1"
-  mail_nickname       = "iac-user1"
-  password            = var.user_password
-  job_title           = "IAM Engineer"
-  department          = "Information Security"
+variable "lab_users" {
+  description = "Map of lab users to create"
+  type = map(object({
+    display_name = string
+    job_title    = string
+    department   = string
+  }))
+  default = {
+    "iac.user1" = {
+      display_name = "IaC Lab User 1"
+      job_title    = "IAM Engineer"
+      department   = "Information Security"
+    }
+    "iac.user2" = {
+      display_name = "IaC Lab User 2"
+      job_title    = "Helpdesk Analyst"
+      department   = "IT Operations"
+    }
+    "iac.user3" = {
+      display_name = "IaC Lab User 3"
+      job_title    = "Security Analyst"
+      department   = "Information Security"
+    }
+  }
 }
 
-resource "azuread_user" "lab_user_2" {
-  user_principal_name = "iac.user2@${var.domain_name}"
-  display_name        = "IaC Lab User 2"
-  mail_nickname       = "iac-user2"
+resource "azuread_user" "lab_users" {
+  for_each = var.lab_users
+
+  user_principal_name = "${each.key}@${var.domain_name}"
+  display_name        = each.value.display_name
+  mail_nickname       = each.key
   password            = var.user_password
-  job_title           = "Helpdesk Analyst"
-  department          = "IT Operations"
+  job_title           = each.value.job_title
+  department          = each.value.department
+}
 }
